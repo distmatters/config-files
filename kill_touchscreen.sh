a97fb3dda115b6d7ed08b4318045c3e1f8e3e159
penny k <penny@muelnet.org>
2019-02-02 22:08:46 -0800

    Created a script to kill the touch screen
    Created a script with some help from stack exchange to find the id of the touch
    screen on my laptop then disable it. Then added it to openbox's autostart script

---
+++ kill_touchscreen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+id=$(xinput --list | awk -v search="$ELAN" \
+    '$0 ~ search {match($0, /id=[0-9]+/);\
+                  if (RSTART) \
+                    print substr($0, RSTART+3, RLENGTH-3)\
+                 }'\
+     )
+xinput disable $id
