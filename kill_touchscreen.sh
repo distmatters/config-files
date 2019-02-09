116c471f2df1f907d36a952fd20730b96a47a71a
penny k <penny@muelnet.org>
2019-02-08 19:52:59 -0800

    Fixed a bug which caused the touch screen to not be disabled
    Had a $ at the start of the searched for string to identify the touch screen.
    Removed this and now it actually works.

--- kill_touchscreen.sh
+++ kill_touchscreen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-id=$(xinput --list | awk -v search="$ELAN" \
+id=$(xinput --list | awk -v search="ELAN" \
     '$0 ~ search {match($0, /id=[0-9]+/);\
                   if (RSTART) \
                     print substr($0, RSTART+3, RLENGTH-3)\
