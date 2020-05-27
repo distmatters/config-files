7d96242c41de592ec222c7f1325519191a3f177b
skrawling <790306+skrawling@users.noreply.github.com>
2020-05-27 15:53:54 -0700

    Fixed plugin manager problem

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
